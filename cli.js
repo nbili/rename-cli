05113dbb252ca9bd7b7d6585d2fc22ae1ff97d93
nbili <coding520@163.com>
2022-03-14 23:14:35 +0800

    init

--- cli.js
+++ cli.js
@@ -1,31 +1,42 @@
 #!/usr/bin/env node
 
-import { resolve, basename, parse } from "path";
-import { readdir, rename } from "fs/promises";
+import { resolve, parse } from "path";
+import { readdir, rename, stat } from "fs/promises";
 
-async function* getFiles(dir, reg) {
+async function* getTargets(dir, reg) {
   const dirents = await readdir(dir, { withFileTypes: true });
   for (const dirent of dirents) {
     const res = resolve(dir, dirent.name);
 
     if (dirent.isDirectory()) {
-      yield* getFiles(res, reg);
-    } else {
-      if (reg.test(basename(res))) {
-        yield res;
-      }
+      yield* getTargets(res, reg);
+    }
+
+    if (reg.test(parse(res).base)) {
+      yield res;
     }
   }
 }
 
 void (async () => {
-  for await (const file of getFiles("./", /\.js/)) {
+  for await (const file of getTargets("./", /./)) {
     const { dir, base } = parse(file);
-    const newBase = base.replace(/bar/, "code");
+    const newBase = base.replace(/code$/, "bar");
+
     const oldPath = resolve(dir, base);
     const newPath = resolve(dir, newBase);
 
+    const ignoreDirectory = true;
+
+    if (ignoreDirectory) {
+      const { isDirectory } = await stat(oldPath);
+      if (isDirectory) {
+        continue;
+      }
+    }
+
     const res = await rename(oldPath, newPath);
+
     if (!res) {
       console.log(`success: ${newPath}`);
     }
