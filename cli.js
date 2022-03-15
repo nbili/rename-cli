ae653bc113df1760f0e32b6331f2e3f3c5e11ba1
nbili <coding520@163.com>
2022-03-15 23:28:02 +0800

    init

--- cli.js
+++ cli.js
@@ -1,37 +1,74 @@
 #!/usr/bin/env node
 
 import { resolve, parse } from "path";
-import { readdir, rename, stat } from "fs/promises";
+import { readdir, stat, rename } from "fs/promises";
+import meow from "meow";
 
-async function* getTargets(dir, reg) {
+const cli = meow(
+  `
+	Usage
+	  $ rename <source> <destination>
+
+	Options
+	  --no-directory,--no-d   Ignore rename directory
+    --filters=<filters>    Filter source string
+
+	Examples
+    Rename current files and directory name 'foo' to 'bar'
+	   $ rename 'foo' 'bar'
+`,
+  {
+    importMeta: import.meta,
+    flags: {
+      directory: {
+        type: "boolean",
+        alias: "d",
+        default: true,
+      },
+      filters: {
+        type: "string",
+        default: "",
+      },
+    },
+  }
+);
+
+async function* getTargets(dir, filters) {
   const dirents = await readdir(dir, { withFileTypes: true });
   for (const dirent of dirents) {
     const res = resolve(dir, dirent.name);
 
     if (dirent.isDirectory()) {
-      yield* getTargets(res, reg);
+      yield* getTargets(res, filters);
     }
 
-    if (reg.test(parse(res).base)) {
+    if (parse(res).base.includes(filters)) {
       yield res;
     }
   }
 }
 
 void (async () => {
-  for await (const file of getTargets("./", /./)) {
+  const source = cli.input[0];
+  const destination = cli.input[1];
+  const filters = cli.flags.filters;
+
+  if (!source || !destination) return process.exit(1);
+
+  for await (const file of getTargets(".", filters)) {
     const { dir, base } = parse(file);
-    const newBase = base.replace(/code$/, "bar");
+    const newBase = base.replace(source, destination);
 
     const oldPath = resolve(dir, base);
     const newPath = resolve(dir, newBase);
 
-    const ignoreDirectory = true;
+    const ignoreDirectory = cli.flags.directory;
 
     if (ignoreDirectory) {
-      const { isDirectory } = await stat(oldPath);
-      if (isDirectory) {
-        continue;
+      const ret = await stat(oldPath);
+
+      if (ret.isDirectory()) {
+        break;
       }
     }
 
